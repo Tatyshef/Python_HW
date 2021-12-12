0939251a535542b64bcd1d0987af74e15146543a
Tatyana <tatmalihina@gmail.com>
2021-12-12 13:50:30 +0300

    2_HW_Python

---
+++ 1_HW.py
@@ -0,0 +1,46 @@
+# name = 'Tatyana'
+# print(name, type(name))
+#
+# age = 33
+# print(age, type(age))
+#
+# fl_week = 11.55
+# print(fl_week, type(fl_week))
+#
+# b_type = bytes(33)
+# print(b_type, type(b_type))
+#
+# list_type = [22, 'Tatyana', 'Mila']
+# print(list_type, type(list_type))
+#
+# tuple = (11, 33),
+# print(tuple, type(tuple)),
+#
+# number = {1,22, 34,'Vadim'}
+# print(number, type(number))
+#
+#
+# dict_type = {'key_1': '22',
+#              'key_2': 'Tatyana'}
+# print(dict_type, type(dict_type))
+
+#
+#
+# rep = 'Ruslan'
+# per = 'Ludmila'
+# vse = 'Ruslan' + 'Ludmil
+
+
+# 12)  Вывести в одну строку переменные типа String
+# # # и Integer используя “,” (запятую):
+
+cor = 'Record'
+roc = 44
+print(cor, str(roc))
+#13) Вывести в одну строку переменные типа String
+# # и Integer используя “,” (плюс):
+# pro = 'Toma'
+# pre = 12
+# got = pro + str(12)
+# print(got)
+
